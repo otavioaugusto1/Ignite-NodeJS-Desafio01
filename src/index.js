5d57db4a9ba2f7435eccf753f87aab4da6eddf52
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-31 11:18:34 -0300

    Add some functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,52 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error:"User not found"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
-app.post('/users', (request, response) => {
+app.post('/users', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+  
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push()
+
+  return response.json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {username} = request.headers;
+
+
+  const user = 
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
