2ad4f62a247e054587160572423c7fc29962e85c
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-04-02 14:28:31 -0300

    All todos are fines

--- src/index.js
+++ src/index.js
@@ -77,14 +77,53 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const{title, deadline} = request.body;
+  
+  const user = request.user;
+  const {id} = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({error:"Todo not found"})
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline)
+
+  return response.json(todo);
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const user = request.user;
+  const {id} = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found"})
+  }
+  todo.done = true;
+  return response.json(todo)
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const user = request.user;
+  const {id} = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id)
+
+  if(todoIndex === -1){
+    return response.status(404).json({error: "Todo not found"})
+  }
+
+  user.todos.splice(todoIndex,1);
+  return response.status(204).send()
+
 });
 
 module.exports = app;
